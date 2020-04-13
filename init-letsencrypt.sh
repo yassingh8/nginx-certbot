6e9657689b1047da21729b364e09a4d9cda62e32
yassingh8 <60339033+yassingh8@users.noreply.github.com>
2020-04-14 02:05:03 +0530

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=18.210.136.97
+domains=168.62.191.193
 rsa_key_size=4096
 data_path="./data/certbot"
 email="tipu1997@gmail.com" # Adding a valid address is strongly recommended
