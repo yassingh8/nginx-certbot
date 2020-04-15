699c151e14084d8f97424c672951547508662658
yassingh8 <60339033+yassingh8@users.noreply.github.com>
2020-04-16 02:21:44 +0530

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=ec2-18-210-136-97.compute-1.amazonaws.com
+domains=psigaming.tk
 rsa_key_size=4096
 data_path="./data/certbot"
 email="yashwardhan.singh@publicissapient.com" # Adding a valid address is strongly recommended
