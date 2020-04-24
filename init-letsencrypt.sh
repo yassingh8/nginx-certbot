b816f158a8f23f5200086d07cedaadac353c66b7
yassingh8 <60339033+yassingh8@users.noreply.github.com>
2020-04-24 13:56:09 +0530

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(gamifylearningcentre.com beta.gamifylearningcentre.com app.gamifylearningcentre.com)
+domains=(beta.gamifylearningcentre.com app.gamifylearningcentre.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="yashwardhan.singh@publicissapient.com" # Adding a valid address is strongly recommended
